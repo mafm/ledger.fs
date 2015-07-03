92a68435d4bd8631cb5d95c4126b7e5715ddb4dd
Matthew McDonald <mafm.bitbucket@mcdonald.net.au>
2015-07-03 10:15:09 +0800

    One test for the parser.

--- src/fsharp/Ledger/ParseTest.fs
+++ src/fsharp/Ledger/ParseTest.fs
@@ -1,5 +1,30 @@
 ﻿module ParseTest
 
+open NUnit.Framework
+open FsUnit
+open Parse
+open Types
+
+[<TestFixture>]
+type ``Test Parsing of transaction text data`` () =
+    [<Test>]
+    member test.``parseTransactionData.`` () =
+        let parse = (parseTransactionFile "2122-22-01 foo\n foo 10AUD\n bar 11\n baz 12\n2012-12-22 foo  sss\nacc $10\n") in do
+            parse |> should equal (ParseSuccess ([Transaction {date = "2122-22-01";
+                                                              description = "foo";
+                                                              postings = [{account = "foo";
+                                                                           amount = AUD 1000;};
+                                                                          {account = "bar";
+                                                                           amount = AUD 1100;};
+                                                                          {account = "baz";
+                                                                           amount = AUD 1200;}];};
+                                                 Transaction {date = "2012-12-22";
+                                                              description = "foo  sss";
+                                                              postings = [{account = "acc";
+                                                                           amount = AUD 1000;}];}]))
+
+
+
 (*
 /////////
 run pTransactionFile "#foo\n\n  #Bar\nVERIFY-BALANCE  2015-01-02  account123 $123 AUD"
@@ -12,10 +37,10 @@ run pMandatorySpace "1 1"
 
 run pOptionalSpace "1"
 run pMandatorySpace "1"
-run pYear "1985"       
-run pMonth " 1985"       
-run pDay " 1985"       
-run pDate "1985-01-01"       
+run pYear "1985"
+run pMonth " 1985"
+run pDay " 1985"
+run pDate "1985-01-01"
 run pCommentLine"#123\n"
 run pOptionalSpace ""
 run pBlankLine ""
@@ -48,7 +73,7 @@ run pPosting "foo -$12\n"
 run pPostings "foo -$12\nbar 10\n"
 
 
-let foo () = 
+let foo () =
      { Posting.account = "456";
        Posting.amount = AUD 10}
 
